beaad0f2b872bac7cf9745853c97bca2042dc0bb
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-12-11 19:14:57 +0000

    redux example

---
+++ src/Counter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import "./Counter.css"
+
+class Counter extends React.Component {
+
+  increment = () => {
+    this.props.dispatch({type: "INCREMENT"})
+  };
+
+  decrement = () => {
+    this.props.dispatch({type: "DECREMENT"})
+  };
+
+  render() {
+    return (
+      <div className ="Counter">
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span className = "count">{this.props.count}</span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  count: state.count
+});
+
+
+export default connect(mapStateToProps) (Counter);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const initialState = {
+  count: 0
+};
+
+//sets the initial state and returns the new state
+//decides what the initial state is and returns new state depending on actions
+//ignores states it doesn't understand
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter/>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById('root'));
+
+
+/*
+import connect from react-redux in component
+
+Connect component to redux
+* Use connect from react-redux 
+  Connect is a higher order function when called returns a fucntion takes in the component
+* Set up mapStateToProps in connect function
+  Tells what data to pull out of the store, mapStateToProps
+
+Create Store
+* Create store function
+* Provide store to App
+* Create a reducer which passes into createStore
+
+Wire Up Actions
+* Store has a function which dispatches actions i.e store.dispatch({type: "INCREMENT"})
+* this passes actions into the reducer
+* connect function injects the dispatch into props
+
+*/
